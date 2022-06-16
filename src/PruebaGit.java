eb160069e5a5d4c23d05c75963a77749fc6aa79c
CristianFernando <gordyfernando@gmail.com>
2022-06-16 13:43:26 -0300

    cree una clase

---
+++ src/PruebaGit.java
@@ -0,0 +1,9 @@
+
+public class PruebaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Probando Controlador de versiones con git");
+	}
+
+}
