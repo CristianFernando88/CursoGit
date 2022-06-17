fd96d09908d5dfe042d8c3482356cdcb5730167f
CristianFernando <gordyfernando@gmail.com>
2022-06-17 11:15:17 -0300

    agregue clase persona

---
+++ src/Persona.java
@@ -0,0 +1,14 @@
+
+public class Persona {
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+}
