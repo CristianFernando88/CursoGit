7a1c77e46f2ee670bf67c7ccc0c14f2b86d0e2a2
CristianFernando88 <107149744+CristianFernando88@users.noreply.github.com>
2022-06-17 11:04:17 -0300

    Update PruebaGit.java

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -4,6 +4,8 @@ public class PruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Probando Controlador de versiones con git");
+		//esitando desde github
+		System.out.println("Texto agregado desde la cuneta de github");
 	}
 
 }
